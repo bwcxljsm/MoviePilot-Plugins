1dd717e55d605aeb255b291f762794a2d10a164c
什么破名字 <59049655+bwcxljsm@users.noreply.github.com>
2024-12-21 13:09:24 +0800

    Update __init__.py

--- plugins/wechatbotmsg/__init__.py
+++ plugins/wechatbotmsg/__init__.py
@@ -15,7 +15,7 @@ class WechatBotMsg(_PluginBase):
     # 插件图标
     plugin_icon = "Wechat_A.png"
     # 插件版本
-    plugin_version = "1.1"
+    plugin_version = "1.2"
     # 插件作者
     plugin_author = "bwcxljsm"
     # 作者主页
